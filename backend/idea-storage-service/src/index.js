506ff021e1db0fdbc2fb349a383295894dd622f7
Dustin Stringer <dustin@negativezeroinc.com>
2024-10-14 14:10:40 -0700

    added morgan for logging

--- backend/idea-storage-service/src/index.js
+++ backend/idea-storage-service/src/index.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import { pool } from './db.js';
 import ideaRoutes from './routes/ideaRoutes.js';
-
+import morgan from 'morgan';
 dotenv.config();
 
 const app = express();
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
-
+app.use(morgan('dev'));
 app.use('/api/ideas', ideaRoutes);
 
 app.listen(PORT, () => {
